f45c7123348fdf6338581436fdbeef8ac0548144
chumpatrol1 <chumpatrol1@gmail.com>
2022-09-20 18:46:14 -0400

    Floored the Chocolate

--- src/ChocolateBars.java
+++ src/ChocolateBars.java
@@ -12,8 +12,10 @@ public class ChocolateBars {
         int age = sc.nextInt();
 
         // Calculate the chocolate bars
-        long woman_chocolate = Math.round((655 + (4.3 * weight) + (4.7 * height) - (4.7 * age))/230);
-        long man_chocolate = Math.round((66 + (6.3 * weight) + (12.9 * height) - (6.8 * age))/230);
+        int woman_chocolate = (int)Math.floor((655 + (4.3 * weight) + (4.7 * height) - (4.7 * age))/230);
+        int man_chocolate = (int)Math.floor((66 + (6.3 * weight) + (12.9 * height) - (6.8 * age))/230);
+
+        // Cast to int
 
         System.out.println("Number of chocolate bars for woman is " + woman_chocolate);
         System.out.println("Number of chocolate bars for man is " + man_chocolate);
