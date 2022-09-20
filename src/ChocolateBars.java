52cde5944ba67130c304f315dea7d21928a99c7a
J9Jones9 <j9jones910@gmail.com>
2022-09-20 19:03:04 -0400

    updating code

--- src/ChocolateBars.java
+++ src/ChocolateBars.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class ChocolateBars {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the weight in pounds: ");
         int weight = sc.nextInt();
@@ -10,10 +10,12 @@ public class ChocolateBars {
         System.out.print("Enter the age in years: ");
         int age = sc.nextInt();
 
-        long woman_chocolate = Math.round((655 + (4.3 * weight) + (4.7 * height) - (4.7 * age))/230);
-        long man_chocolate = Math.round((66 + (6.3 * weight) + (12.9 * height) - (6.8 * age))/230);
+        long woman_chocolate = Math.round((655 + (4.3 * weight) + (4.7 * height) - (4.7 * age)) / 230);
+        long man_chocolate = Math.round((66 + (6.3 * weight) + (12.9 * height) - (6.8 * age)) / 230);
 
         System.out.println("Number of chocolate bars for woman is " + woman_chocolate);
         System.out.println("Number of chocolate bars for man is " + man_chocolate);
+
+        sc.close();
     }
 }
