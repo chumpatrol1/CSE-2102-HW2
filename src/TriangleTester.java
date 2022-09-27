eddccf7ec0706ea14516f4790253b2499f80c7b8
chumpatrol1 <chumpatrol1@gmail.com>
2022-09-27 17:49:34 -0400

    Triangle Tester

---
+++ src/TriangleTester.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.lang.*;
+import java.util.Arrays;
+
+public class TriangleTester {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Side A: ");
+        double a_side = sc.nextDouble();
+        System.out.print("Side B: ");
+        double b_side = sc.nextDouble();
+        System.out.print("Side C: ");
+        double c_side = sc.nextDouble();
+
+        if(a_side + b_side > c_side && b_side + c_side > a_side && a_side + c_side > b_side){
+            System.out.println("These lengths can form a triangle");
+
+            double a_squared = a_side * a_side;
+            double b_squared = b_side * b_side;
+            double c_squared = c_side * c_side;
+            double ordered_by_size[] = new double[]{a_squared, b_squared, c_squared};
+            Arrays.sort(ordered_by_size);
+
+            /**
+            for (double num : ordered_by_size){
+                System.out.println(num);
+            }
+            */
+            
+
+            if(a_squared + b_squared == c_squared || a_squared + c_squared == b_squared || b_squared + c_squared == a_squared){
+                System.out.println("It is a right triangle");
+            } else if (ordered_by_size[0] + ordered_by_size[1] > ordered_by_size[2]){
+                System.out.println("It is an obtuse triangle");
+            } else {
+                System.out.println("It is an acute triangle");
+            }
+
+
+        } else {
+            System.out.println("These lengths cannot form a triangle");
+        }
+    }
+}
