7392943a6ae31dbe819b5ba68321f39c53624804
J9Jones9 <j9jones910@gmail.com>
2022-10-05 11:52:53 -0400

    fixing up averager

--- src/ExamAverager.java
+++ src/ExamAverager.java
@@ -10,28 +10,28 @@ public class ExamAverager {
         System.out.println("Enter a negative number after you have entered all the scores.");
 
         // initializing an array to store exam value
-        int intArray[];
-        intArray = new int[100];
+        double floatArray[];
+        floatArray = new double[100];
         // need to keep track of total exams to get an average
         int count = 0;
-        int newValue;
-        int tempTotal = 0;
-        float examAverage = 0;
+        double newValue;
+        double tempTotal = 0;
+        double examAverage = 0;
 
         // need to change array to -1 because what if someone scores 0
-        for (int i = 0; i < intArray.length; i++) {
-            intArray[i] = -1;
+        for (int i = 0; i < floatArray.length; i++) {
+            floatArray[i] = -1;
         }
 
         // getting our first average
         while (true) {
             // keep going until a negative input
-            newValue = (sc.nextInt());
+            newValue = (sc.nextDouble());
             if (newValue < 0) {
                 break;
             } else {
                 // if not negative change array and incease count
-                intArray[count] = newValue;
+                floatArray[count] = newValue;
                 count += 1;
                 tempTotal += newValue;
 
@@ -39,22 +39,29 @@ public class ExamAverager {
 
         }
         // calculating average
-        examAverage = (float) tempTotal / count;
+        examAverage = (double) tempTotal / (double) count;
         System.out.println("The average is " + examAverage);
 
+        // cur keeps track of where we are in the array
         int cur = 0;
         while (true) {
             // if array gets to -1 then that is the end of what was inputted
-            if (intArray[cur] <= -1) {
+            if (floatArray[cur] <= -1) {
                 break;
             } else {
                 // formatting output based on score
-                if (intArray[cur] < examAverage) {
-                    System.out.println("Score #" + (cur + 1) + ": " + intArray[cur] + " -- " + "Below average");
-                } else if (intArray[cur] > examAverage) {
-                    System.out.println("Score #" + (cur + 1) + ": " + intArray[cur] + " -- " + "Above average");
+                if (floatArray[cur] < examAverage) {
+                    System.out.print("Score #" + (cur + 1) + ": ");
+                    System.out.printf("%6f", floatArray[cur]);
+                    System.out.println(" -- Below average");
+                } else if (floatArray[cur] > examAverage) {
+                    System.out.print("Score #" + (cur + 1) + ": ");
+                    System.out.printf("%6f", floatArray[cur]);
+                    System.out.println(" -- Above average");
                 } else {
-                    System.out.println("Score #" + (cur + 1) + ": " + intArray[cur] + " -- " + "average");
+                    System.out.print("Score #" + (cur + 1));
+                    System.out.printf("%6f", floatArray[cur]);
+                    System.out.println(" -- Below average");
                 }
 
                 cur += 1;
@@ -87,8 +94,8 @@ public class ExamAverager {
                 tempTotal = 0;
 
                 // need to reset array
-                for (int i = 0; i < intArray.length; i++) {
-                    intArray[i] = -1;
+                for (int i = 0; i < floatArray.length; i++) {
+                    floatArray[i] = -1;
                 }
                 while (true) {
                     // keep going until a negative input
@@ -97,7 +104,7 @@ public class ExamAverager {
                         break;
                     } else {
                         // if not negative change array and incease count
-                        intArray[count] = newValue;
+                        floatArray[count] = newValue;
                         count += 1;
                         tempTotal += newValue;
 
@@ -110,17 +117,23 @@ public class ExamAverager {
                 // cur keeps track of what number exam is being printed
                 cur = 0;
                 while (true) {
-                    if (intArray[cur] <= -1) {
+                    if (floatArray[cur] <= -1) {
                         break;
                     } else {
-                        if (intArray[cur] < examAverage) {
-                            System.out.println("Score #" + (cur + 1) + ": " + intArray[cur] + " -- " + "Below average");
-                        } else if (intArray[cur] > examAverage) {
-                            System.out.println("Score #" + (cur + 1) + ": " + intArray[cur] + " -- " + "Above average");
+                        if (floatArray[cur] < examAverage) {
+                            System.out.print("Score #" + (cur + 1) + ": ");
+                            System.out.printf("%6f", floatArray[cur]);
+                            System.out.println(" -- Below average");
+                        } else if (floatArray[cur] > examAverage) {
+                            System.out.print("Score #" + (cur + 1) + ": ");
+                            System.out.printf("%6f", floatArray[cur]);
+                            System.out.println(" -- Above average");
                         } else {
-                            System.out.println("Score #" + (cur + 1) + ": " + intArray[cur] + " -- " + "average");
+                            System.out.print("Score #" + (cur + 1) + ": ");
+                            System.out.printf("%6f", floatArray[cur]);
+                            System.out.println(" -- Average");
                         }
-
+                        // update cur to go to the next exam
                         cur += 1;
 
                     }
